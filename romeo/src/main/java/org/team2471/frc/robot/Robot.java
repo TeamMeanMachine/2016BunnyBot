97694d0dd7707ed4660232f5d4f180d0d0a692a9
tylerscheuble <tylerscheuble@gmail.com>
2016-09-27 19:13:15 -0700

    Set up template for both robots

---
+++ romeo/src/main/java/org/team2471/frc/robot/Robot.java
@@ -0,0 +1,26 @@
+package org.team2471.frc.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+  /**
+   * Robot-wide initialization code should go here.
+   */
+  @Override
+  public void robotInit() {
+  }
+
+  /**
+   * Initialization code for teleop mode should go here.
+   */
+  @Override
+  public void teleopInit() {
+  }
+
+  /**
+   * Periodic code for teleop mode should go here.
+   */
+  @Override
+  public void teleopPeriodic() {
+  }
+}
