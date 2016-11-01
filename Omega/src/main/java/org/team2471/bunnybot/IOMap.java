9d6747271880dd0fb45ac4a4998c3ce6c913914c
tylerscheuble <tylerscheuble@gmail.com>
2016-10-31 22:29:27 -0700

    Created IOMap

---
+++ Omega/src/main/java/org/team2471/bunnybot/IOMap.java
@@ -0,0 +1,9 @@
+package org.team2471.bunnybot;
+
+import org.team2471.frc.lib.control.DriveAxis;
+import org.team2471.frc.lib.control.DriveController;
+
+public class IOMap {
+  private static final DriveController mainController = new DriveController(0);
+  private static final DriveController coController = new DriveController(1);
+}
