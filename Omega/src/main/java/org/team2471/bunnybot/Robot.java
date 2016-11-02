d6acc897582927a5354f9f77b6d4b0afb9077ef8
tylerscheuble <tylerscheuble@gmail.com>
2016-11-01 19:12:47 -0700

    Declared subsystems in robot

--- Omega/src/main/java/org/team2471/bunnybot/Robot.java
+++ Omega/src/main/java/org/team2471/bunnybot/Robot.java
@@ -1,7 +1,11 @@
 package org.team2471.bunnybot;
 
+import org.team2471.bunnybot.subsystems.DriveTrain;
+import org.team2471.bunnybot.subsystems.Grabber;
 import org.team2471.bunnybot.subsystems.Shooter;
 
 public class Robot {
-    public static Shooter shooter;
+  public static Shooter shooter;
+  public static DriveTrain driveTrain;
+  public static Grabber grabber;
 }
