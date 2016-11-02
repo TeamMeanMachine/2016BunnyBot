e4cc1dde862e4fc49cafb2fd3b547e7662ca0cd3
James-Kling <klingjt@gmail.com>
2016-11-01 19:08:10 -0700

    Created empty default command for drive train

---
+++ Omega/src/main/java/org/team2471/bunnybot/defaultcommands/DriveTrainDefaultCommand.java
@@ -0,0 +1,30 @@
+package org.team2471.bunnybot.defaultcommands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveTrainDefaultCommand extends Command {
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
