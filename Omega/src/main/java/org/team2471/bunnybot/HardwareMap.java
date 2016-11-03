ede576d5466b3248d39181559bcb0d2525e551a2
tylerscheuble <tylerscheuble@gmail.com>
2016-11-02 19:32:22 -0700

    Fixed issues preventing deploying

--- Omega/src/main/java/org/team2471/bunnybot/HardwareMap.java
+++ Omega/src/main/java/org/team2471/bunnybot/HardwareMap.java
@@ -3,7 +3,6 @@ package org.team2471.bunnybot;
 import edu.wpi.first.wpilibj.*;
 
 public class HardwareMap {
-  // TODO: Provide proper IDs
   public static final class ShooterMap {
     public static final CANTalon panMotor = new CANTalon(0);
     public static final SpeedController shootMotor = new CANTalon(0);
@@ -12,28 +11,28 @@ public class HardwareMap {
     public static final AnalogInput ammoSensor = new AnalogInput(0);
     public static final Solenoid flashLight = new Solenoid(0);
   }
-
-  public static final class DriveTrainMap {
-    public static final class LeftModule {
-      public static final AnalogInput turnEncoder = new AnalogInput(0);
-      public static final SpeedController turnMotor = new CANTalon(0);
-      public static final CANTalon forwardMotor = new CANTalon(0);
-    }
-
-    public static final class RightModule {
-      public static final AnalogInput turnEncoder = new AnalogInput(0);
-      public static final SpeedController turnMotor = new CANTalon(0);
-      public static final CANTalon forwardMotor = new CANTalon(0);
-    }
-
-    public static final SpeedController frontLeftMotor = new CANTalon(0);
-    public static final SpeedController frontRightMotor = new CANTalon(0);
-    public static final SpeedController backLeftMotor = new CANTalon(0);
-    public static final SpeedController backRightMotor = new CANTalon(0);
-  }
-
-  // TODO: complete this
-  public static final class GrabberMap {
-
-  }
+//
+//  public static final class DriveTrainMap {
+//    public static final class LeftModule {
+//      public static final AnalogInput turnEncoder = new AnalogInput(0);
+//      public static final SpeedController turnMotor = new CANTalon(0);
+//      public static final CANTalon forwardMotor = new CANTalon(0);
+//    }
+//
+//    public static final class RightModule {
+//      public static final AnalogInput turnEncoder = new AnalogInput(0);
+//      public static final SpeedController turnMotor = new CANTalon(0);
+//      public static final CANTalon forwardMotor = new CANTalon(0);
+//    }
+//
+//    public static final SpeedController frontLeftMotor = new CANTalon(0);
+//    public static final SpeedController frontRightMotor = new CANTalon(0);
+//    public static final SpeedController backLeftMotor = new CANTalon(0);
+//    public static final SpeedController backRightMotor = new CANTalon(0);
+//  }
+//
+//  // TODO: complete this
+//  public static final class GrabberMap {
+//
+//  }
 }
--- Omega/src/main/java/org/team2471/bunnybot/Robot.java
+++ Omega/src/main/java/org/team2471/bunnybot/Robot.java
@@ -4,8 +4,15 @@ import org.team2471.bunnybot.subsystems.DriveTrain;
 import org.team2471.bunnybot.subsystems.Grabber;
 import org.team2471.bunnybot.subsystems.Shooter;
 
-public class Robot {
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
   public static Shooter shooter;
   public static DriveTrain driveTrain;
   public static Grabber grabber;
+
+  @Override
+  public void robotInit() {
+    shooter = new Shooter();
+  }
 }
