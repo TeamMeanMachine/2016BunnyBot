84f77bb4df07dcdc0570755e13e3480af569513c
James-Kling <klingjt@gmail.com>
2016-11-30 20:05:21 -0800

    got shooter working

--- Omega/src/main/java/org/team2471/bunnybot/subsystems/Shooter.java
+++ Omega/src/main/java/org/team2471/bunnybot/subsystems/Shooter.java
@@ -1,61 +1,41 @@
 package org.team2471.bunnybot.subsystems;
 
 import org.team2471.bunnybot.HardwareMap;
+import org.team2471.bunnybot.defaultcommands.ShooterDefaultCommand;
+import org.team2471.util.DummySubsystem;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends Subsystem {
-  private final CANTalon panMotor = HardwareMap.ShooterMap.panMotor;
-  private final SpeedController shootMotor = HardwareMap.ShooterMap.shootMotor;
-  private final AnalogGyro shooterGyro = HardwareMap.ShooterMap.shooterGyro;
-  private final Servo tiltMotor = HardwareMap.ShooterMap.tiltMotor;
-  private final AnalogInput ammoSensor = HardwareMap.ShooterMap.ammoSensor;
-  private final Solenoid flashLight = HardwareMap.ShooterMap.flashLight;
+//  private final CANTalon panMotor = HardwareMap.ShooterMap.panMotor;
+//  private final SpeedController shootMotor = HardwareMap.ShooterMap.shootMotor;
+//  private final AnalogGyro shooterGyro = HardwareMap.ShooterMap.shooterGyro;
+//  private final Servo tiltMotor = HardwareMap.ShooterMap.tiltMotor;
+//  private final DigitalInput ammoSensor = HardwareMap.ShooterMap.ammoSensor;
+//  private final Solenoid flashLight = HardwareMap.ShooterMap.flashLight;
 
-  public final Subsystem trigger = new Subsystem() {
-    @Override
-    protected void initDefaultCommand() {
-    }
+  private final Talon panMotor = new Talon(3);
+  private final SpeedController shootMotor = new Talon(5);
+  private final AnalogGyro shooterGyro = new AnalogGyro(0);
+  private final Servo tiltMotor = new Servo(9);
+  private final DigitalInput ammoSensor = new DigitalInput(9);
+  private final Solenoid flashLight = new Solenoid(0);
 
-    /**
-     * use boolean to make sure that the shooter motor has finished its shooting
-     * iteration and has completely returned to standby position ready for next shot
-     *
-     * @return a boolean that check the completion as stated above
-     */
-    public boolean shooterMotorReady() {
-      return ammoSensor.isAccumulatorChannel();
-    }
+  public final Subsystem trigger = new DummySubsystem();
 
-    /**
-     * Runs the shooting motors.
-     *
-     * It is expected that the turret continues firing in full auto until
-     * shooting is disabled.
-     */
-    public void enable() {
-      double shooterPower = shooterMotorReady() ? 0.5 : 0;
-      shootMotor.set(shooterPower);
-    }
-
-    /**
-     * Disables the shooting motors.
-     */
-    public void disableShooting() {
-      shootMotor.set(-0.5);
-    }
-  };
-
-  private final PIDController panController = new PIDController(0, 0, 0, new PIDSource() {
+  private final PIDController panController = new PIDController(0.01, 0, 0, new PIDSource() {
     @Override
     public void setPIDSourceType(PIDSourceType pidSource) {
     }
@@ -69,15 +49,19 @@ public class Shooter extends Subsystem {
     public double pidGet() {
       return getAngle();
     }
-  }, panMotor);
+  }, panMotor );
 
   public Shooter() {
+    panController.setInputRange(-180, 180);
+    panController.setOutputRange(-1, 1);
+    panController.setContinuous(true);
     panController.enable();
+    SmartDashboard.putData("PanController", panController);
   }
 
   @Override
   protected void initDefaultCommand() {
-
+    setDefaultCommand(new ShooterDefaultCommand());
   }
 
   /**
@@ -85,10 +69,41 @@ public class Shooter extends Subsystem {
    */
   public double getAngle() {
     double shooterGyroAngle = shooterGyro.getAngle();
-    if (shooterGyroAngle > 0) {
-      shooterGyroAngle += 360;
-    }
+    System.out.println("Angle: " + shooterGyroAngle + " (" + shooterGyroAngle % 360 + ")");
+//    if (shooterGyroAngle > 0) {
+//      shooterGyroAngle += 360;
+//    }
     return shooterGyroAngle % 360;
+  }
+
+
+  /**
+   * use boolean to make sure that the shooter motor has finished its shooting
+   * iteration and has completely returned to standby position ready for next shot
+   *
+   * @return a boolean that check the completion as stated above
+   */
+  public boolean isReady() {
+    return ammoSensor.get();
+  }
+
+  /**
+   * Runs the shooting motors.
+   *
+   * It is expected that the turret continues firing in full auto until
+   * shooting is disabled.
+   */
+  public void enableShooting() {
+    //double shooterPower = isReady() ? 0.5 : 0;
+    double shooterPower = 0.5;
+    shootMotor.set(shooterPower);
+  }
+
+  /**
+   * Disables the shooting motors.
+   */
+  public void disableShooting() {
+    shootMotor.set(0);
   }
 
   /**
@@ -98,6 +113,11 @@ public class Shooter extends Subsystem {
    */
   public void setAngle(double angle) {
     panController.setSetpoint(angle);
+  }
+
+
+  public void setTilt(double angle){
+    tiltMotor.setAngle(angle);
   }
 
   /**
