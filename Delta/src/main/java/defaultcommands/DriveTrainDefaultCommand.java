1c7db4475cbc481339ac29e14d5bd6cc92f13e9a
lukeg32 <lukegantar@gmail.com>
2016-12-05 19:49:33 -0800

    added drive train , and defaulcommand for drivetrain

---
+++ Delta/src/main/java/defaultcommands/DriveTrainDefaultCommand.java
@@ -0,0 +1,27 @@
+package defaultcommands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveTrainDefaultCommand extends Command{
+
+  @Override
+  protected void initialize() {
+
+  }
+  @Override
+  protected void execute() {
+
+  }
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+  @Override
+  protected void end() {
+
+  }
+  @Override
+  protected void interrupted() {
+
+  }
+}
