c092d431bcdb3098c990dc06cc38b7d7c7c22089
Justine P <justinetulip@gmail.com>
2016-12-06 19:39:05 -0800

    Added smart dashboard stuff

--- Delta/src/main/java/org/team2471/bunnybot/subsystem/Arm.java
+++ Delta/src/main/java/org/team2471/bunnybot/subsystem/Arm.java
@@ -6,8 +6,6 @@ import org.team2471.bunnybot.util.MagnepotPIDSource;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDSource;
-import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm {
@@ -21,19 +19,28 @@ public class Arm {
   private PIDController elbowPID = new PIDController(0,0,0, new MagnepotPIDSource(elbowEncoder),elbowMotor);
 
   public Arm() {
+    SmartDashboard.putData("Shoulder PID", shoulderPID);
+    SmartDashboard.putData("Elbow PID", elbowPID);
   }
+  /** Sets the shoulder to a specific angle*/
   public void setShoulderAngle(double angle) {shoulderPID.setSetpoint(angle);}
 
+  /** Sets the elbow to a specific angle */
   public void setElbowAngle(double angle) {elbowPID.setSetpoint(angle);}
 
+  /** Gets the shoulder angle*/
   public double getShoulderAngle() {return (shoulderEncoder.getVoltage()-2.5)/2.3*180;}
 
+  /** Gets the elbow angle*/
   public double getElbowAngle() {return (elbowEncoder.getVoltage()-2.5)/2.3*180;}
 
+  /** Sucks the bunny in*/
   public void suckIn() {bunnySucker.set(1);}
 
+  /** Spits the bunny out*/
   public void spitOut() {bunnySucker.set(-1);}
 
+  /** Stops the intake*/
   public void stopIntake() {bunnySucker.set(0);}
 
 
