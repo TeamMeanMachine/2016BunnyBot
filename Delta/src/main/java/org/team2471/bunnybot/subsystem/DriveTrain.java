1d9e7f75a88e23fccebb2ee6fc4b7ee16fc9a554
lukeg32 <lukegantar@gmail.com>
2016-12-07 20:48:08 -0800

    added cheesydrive

--- Delta/src/main/java/org/team2471/bunnybot/subsystem/DriveTrain.java
+++ Delta/src/main/java/org/team2471/bunnybot/subsystem/DriveTrain.java
@@ -1,12 +1,16 @@
 package org.team2471.bunnybot.subsystem;
 
+import com.team254.frc2016.CheesyDriveHelper;
+import com.team254.lib.util.DriveSignal;
 import defaultcommands.DriveTrainDefaultCommand;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import static org.team2471.bunnybot.HardwareMap.Drivetrain.*;
 
 public class DriveTrain extends Subsystem {
+  private CheesyDriveHelper cheesyDriveHelper;
 
   public DriveTrain() {
     rightMotor2.changeControlMode(CANTalon.TalonControlMode.Follower);
@@ -18,20 +22,29 @@ public class DriveTrain extends Subsystem {
     leftMotor3.changeControlMode(CANTalon.TalonControlMode.Follower);
     leftMotor2.set(leftMotor1.getDeviceID());
     leftMotor3.set(leftMotor1.getDeviceID());
+
+    cheesyDriveHelper = new CheesyDriveHelper();
+  }
+
+  public void drive(double throttle, double turn) {
+   if (SmartDashboard.getBoolean("CheesyDrive", false)) {
+     DriveSignal driveSignal = cheesyDriveHelper.cheesyDrive(throttle, turn, false);
+
+     rightMotor1.set(driveSignal.rightMotor);
+     leftMotor1.set(driveSignal.leftMotor);
+   }
+   else {
+     double left = throttle + turn;
+     double right = throttle - turn;
+
+     rightMotor1.set(right);
+     leftMotor1.set(left);
+   }
   }
 
   @Override
   protected void initDefaultCommand() {
     setDefaultCommand(new DriveTrainDefaultCommand());
-  }
-
-  public void drive(double throttle, double turn) {
-    double left = throttle + turn;
-    double right = throttle - turn;
-
-    rightMotor1.set(right);
-    leftMotor1.set(left);
-
   }
 
 
