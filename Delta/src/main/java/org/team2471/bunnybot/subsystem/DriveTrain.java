b69f179ecf1065db931358c168f964dc51121866
bobcroucher <bobc@bentimagelab.com>
2016-12-10 17:20:46 -0800

    Drive Train to a working state

--- Delta/src/main/java/org/team2471/bunnybot/subsystem/DriveTrain.java
+++ Delta/src/main/java/org/team2471/bunnybot/subsystem/DriveTrain.java
@@ -1,7 +1,7 @@
 package org.team2471.bunnybot.subsystem;
 
-import com.team254.frc2016.CheesyDriveHelper;
-import com.team254.lib.util.DriveSignal;
+//import com.team254.frc2016.CheesyDriveHelper;
+//import com.team254.lib.util.DriveSignal;
 import defaultcommands.DriveTrainDefaultCommand;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -10,9 +10,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import static org.team2471.bunnybot.HardwareMap.Drivetrain.*;
 
 public class DriveTrain extends Subsystem {
-  private CheesyDriveHelper cheesyDriveHelper;
+  //private CheesyDriveHelper cheesyDriveHelper;
+  private static final double LOWSHIFTPOINT = 6.0;
+  private static final double HIGHSHIFTPOINT = 8.0;
 
   public DriveTrain() {
+
+    //rightMotor1.reverseOutput(true);
     rightMotor2.changeControlMode(CANTalon.TalonControlMode.Follower);
     rightMotor3.changeControlMode(CANTalon.TalonControlMode.Follower);
     rightMotor2.set(rightMotor1.getDeviceID());
@@ -23,23 +27,29 @@ public class DriveTrain extends Subsystem {
     leftMotor2.set(leftMotor1.getDeviceID());
     leftMotor3.set(leftMotor1.getDeviceID());
 
-    cheesyDriveHelper = new CheesyDriveHelper();
+    //cheesyDriveHelper = new CheesyDriveHelper();
   }
 
   public void drive(double throttle, double turn) {
-   if (SmartDashboard.getBoolean("CheesyDrive", false)) {
+/*   if (SmartDashboard.getBoolean("CheesyDrive", false)) {
      DriveSignal driveSignal = cheesyDriveHelper.cheesyDrive(throttle, turn, false);
 
      rightMotor1.set(driveSignal.rightMotor);
      leftMotor1.set(driveSignal.leftMotor);
    }
-   else {
+   else {*/
      double left = throttle + turn;
      double right = throttle - turn;
 
-     rightMotor1.set(right);
+     rightMotor1.set(-right);
      leftMotor1.set(left);
-   }
+//   }
+    if (leftMotor1.getSpeed() > HIGHSHIFTPOINT) {
+      shiftSolenoid.set(true);
+    }
+    else if(leftMotor1.getSpeed() < LOWSHIFTPOINT) {
+      shiftSolenoid.set(false);
+    }
   }
 
   @Override
